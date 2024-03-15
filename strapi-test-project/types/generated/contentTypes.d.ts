2e05d0581e4c29092476cd2a35a4d9c2f3285d7b
Henrique Doro <opensource@hdoro.dev>
2024-03-15 14:40:05 -0300

    update demo

--- strapi-test-project/types/generated/contentTypes.d.ts
+++ strapi-test-project/types/generated/contentTypes.d.ts
@@ -556,6 +556,10 @@ export interface PluginContentReleasesRelease extends Schema.CollectionType {
     releasedAt: Attribute.DateTime;
     scheduledAt: Attribute.DateTime;
     timezone: Attribute.String;
+    status: Attribute.Enumeration<
+      ['ready', 'blocked', 'failed', 'done', 'empty']
+    > &
+      Attribute.Required;
     actions: Attribute.Relation<
       'plugin::content-releases.release',
       'oneToMany',
@@ -610,6 +614,7 @@ export interface PluginContentReleasesReleaseAction
       'manyToOne',
       'plugin::content-releases.release'
     >;
+    isEntryValid: Attribute.Boolean;
     createdAt: Attribute.DateTime;
     updatedAt: Attribute.DateTime;
     createdBy: Attribute.Relation<
